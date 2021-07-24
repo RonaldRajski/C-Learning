50a51815499c337d93328e080777cf1a16b6a25e
Ronald Rajski <ronaldrajski@gmail.com>
2021-07-24 10:04:47 -0500

    drawings

--- Program.cs
+++ Program.cs
@@ -12,14 +12,34 @@ namespace HelloWorld
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
-            // Console.WriteLine("What is your name?");
-            
-            // var name = Console.ReadLine();
-            // var curentDate = DateTime.Now;
-            // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            // Console.Write($"{Environment.NewLine} Press any key to exit...");
-            // Console.ReadKey(true);
 
-        }
+        Console.WriteLine(" .-.");
+      // This is a redundant code
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Johnnie Da Ghost");
+
+
+      Console.WriteLine("c_      ___");
+      Console.WriteLine("   \\\\__|__|__");
+      Console.WriteLine("    \\( o_o)");
+      Console.WriteLine("      > ~  >");
+      Console.WriteLine("     /    /\\\\");
+      Console.WriteLine("    /    /  \\\\");
+      Console.WriteLine("    \\    )  c/");
+      Console.WriteLine("    /   /");
+      Console.WriteLine("   /   /|");
+      Console.WriteLine("   (   (\\ ");
+      Console.WriteLine("   |   | \\ ");
+      Console.WriteLine("   |  / \\ ) ");
+      Console.WriteLine("   |  |  ) | ");
+      Console.WriteLine("  /   )(_/");
+      Console.WriteLine("(_ /");
+
+
+
+      }
     }
 }
